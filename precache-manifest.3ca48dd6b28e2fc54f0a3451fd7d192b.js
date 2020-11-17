7f3fda1c4e4054fe5bf517f9ff710c70c7b7610f
SoftwareEngAhmetDemir <deepdamer@gmail.com>
2020-11-17 12:57:27 +0300

    Updates

---
+++ precache-manifest.3ca48dd6b28e2fc54f0a3451fd7d192b.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7c46d41c8b00b6233949699d1b9536",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/index.html"
+  },
+  {
+    "revision": "14f35d777ce0ab3bac63",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/css/0.22579499.chunk.css"
+  },
+  {
+    "revision": "0a199d37ff7ad28b101b",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/css/3.404718ee.chunk.css"
+  },
+  {
+    "revision": "ebd783d32bec1fc16514",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/css/5.7d612635.chunk.css"
+  },
+  {
+    "revision": "8284f7e2fba71d142aa7",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/css/main.c6257834.chunk.css"
+  },
+  {
+    "revision": "14f35d777ce0ab3bac63",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/0.11a71ecb.chunk.js"
+  },
+  {
+    "revision": "d21f97c4aeedb0306549",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/10.ef0ad34c.chunk.js"
+  },
+  {
+    "revision": "f78341d37118a4d53647",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/11.7e767998.chunk.js"
+  },
+  {
+    "revision": "8a8b1e91fb6d41b8cd7d",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/12.45bf0d13.chunk.js"
+  },
+  {
+    "revision": "bde45071dff9c24abb85",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/13.27e1216f.chunk.js"
+  },
+  {
+    "revision": "2772c721df70194cf9fc",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/14.034606ac.chunk.js"
+  },
+  {
+    "revision": "3d5e7cfcd4a56ccafbb5",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/15.d05533cd.chunk.js"
+  },
+  {
+    "revision": "704782bf66e3c65f1ea0",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/16.7c0af0fd.chunk.js"
+  },
+  {
+    "revision": "20b87880f358217a1b44",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/17.1f023f9b.chunk.js"
+  },
+  {
+    "revision": "b861d29760f851d87653",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/18.ae0314e6.chunk.js"
+  },
+  {
+    "revision": "62d38d8644564ffa2b67",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/19.3fdb18be.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/19.3fdb18be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f5069a47b8ba69375d4",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/20.59d3b0c2.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/20.59d3b0c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e15ca378bb77d1dcab55",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/21.34132b8a.chunk.js"
+  },
+  {
+    "revision": "0a199d37ff7ad28b101b",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/3.b0cdc3b1.chunk.js"
+  },
+  {
+    "revision": "646b842ab2e3750f58cd3835df65fe32",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/3.b0cdc3b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606dcdb7603fdca47d03",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/4.edcf79c3.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/4.edcf79c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebd783d32bec1fc16514",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/5.4f061ccd.chunk.js"
+  },
+  {
+    "revision": "c47fb89f944fc413937f1d857df6495a",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/5.4f061ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5658ec6a03f59c035462",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/6.68825f87.chunk.js"
+  },
+  {
+    "revision": "3857e9a301df01a99b84",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/7.f8098ad8.chunk.js"
+  },
+  {
+    "revision": "2e5bc72498d7e6a4d708",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/8.0420187e.chunk.js"
+  },
+  {
+    "revision": "abe11c48996452ea2a82",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/9.51f83c1f.chunk.js"
+  },
+  {
+    "revision": "8284f7e2fba71d142aa7",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/main.ef984b45.chunk.js"
+  },
+  {
+    "revision": "8a79a3b0265c6bf287dc",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/js/runtime-main.eb53590f.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "95d852eef259d8284d2b6e733022411d",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/Urun_Yok_Banner.95d852ee.jpg"
+  },
+  {
+    "revision": "01364745d98dfde4673967cdef77cd50",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/banner.01364745.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "8300bd7f30e0a313c1d772b49d96cb8e",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/fa-brands-400.8300bd7f.ttf"
+  },
+  {
+    "revision": "ad527cc5ec23d6da66e8a1d6772ea6d3",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/fa-brands-400.ad527cc5.woff"
+  },
+  {
+    "revision": "bfd29c01b47b76d7b1c1411d5b57aa27",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/fa-brands-400.bfd29c01.svg"
+  },
+  {
+    "revision": "e2ca6541bff3a3e9f4799ee327b28c58",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/fa-brands-400.e2ca6541.eot"
+  },
+  {
+    "revision": "f075c50f89795e4cdb4d45b51f1a6800",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/fa-brands-400.f075c50f.woff2"
+  },
+  {
+    "revision": "3c6879c4f342203d099bdd66dce6d396",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/fa-regular-400.3c6879c4.woff"
+  },
+  {
+    "revision": "49f00693b0e5d45097832ef5ea1bc541",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/fa-regular-400.49f00693.ttf"
+  },
+  {
+    "revision": "4a74738e7728e93c4394b8604081da62",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/fa-regular-400.4a74738e.woff2"
+  },
+  {
+    "revision": "519019089275671cad781bb9d0fa98bc",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/fa-regular-400.51901908.svg"
+  },
+  {
+    "revision": "b01516c1808be557667befec76cd6318",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/fa-regular-400.b01516c1.eot"
+  },
+  {
+    "revision": "205f07b3883c484f27f40d21a92950d4",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/fa-solid-900.205f07b3.ttf"
+  },
+  {
+    "revision": "4451e1d86df7491dd874f2c41eee1053",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/fa-solid-900.4451e1d8.woff"
+  },
+  {
+    "revision": "62528c56ff6d27f22dd5c6c8c5bd3d20",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/fa-solid-900.62528c56.svg"
+  },
+  {
+    "revision": "8ac3167427b1d5d2967646bd8f7a0587",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/fa-solid-900.8ac31674.eot"
+  },
+  {
+    "revision": "8e1ed89b6ccb8ce41faf5cb672677105",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/fa-solid-900.8e1ed89b.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/SoftwareEngAhmetDemir/uploadedReact/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
